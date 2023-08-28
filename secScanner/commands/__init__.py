93f51bf0e8aeaed938de18c13458b887d5b25173
pengyuan-yewu <pengyuan_yewu@cmss.chinamobile.com>
2023-08-28 14:16:46 +0800

    add commands/basic and check_outprint file

---
+++ secScanner/commands/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+'''
+   Copyright (c) 2023. China Mobile(SuZhou)Software Technology Co.,Ltd. All rights reserved.
+   secScanner is licensed under Mulan PSL v2.
+   You can use this software according to the terms and conditions of the Mulan PSL v2.
+   You may obtain a copy of Mulan PSL v2 at:
+            http://license.coscl.org.cn/MulanPSL2
+   THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+   See the Mulan PSL v2 for more details.
+'''
+
+
+from .basic import *
+from .check_outprint import check_myid, check_param,  check_pid, display_info, find_profile, check_isvirtualmachine, end_parase
