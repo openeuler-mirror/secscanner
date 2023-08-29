4f6ee82535bbde8205dd2c9cceb9c87d43f74b46
gaojiazhen <gaojiazhen_yewu@cmss.chinamobile.com>
2023-08-29 02:05:06 +0000

    update secscanner.py.
    Signed-off-by: gaojiazhen <gaojiazhen_yewu@cmss.chinamobile.com>

--- secscanner.py
+++ secscanner.py
@@ -3,7 +3,7 @@
 
 '''
    Copyright (c) 2023. China Mobile(SuZhou)Software Technology Co.,Ltd. All rights reserved.
-   SecScanner is licensed under Mulan PSL v2.
+   secScanner is licensed under Mulan PSL v2.
    You can use this software according to the terms and conditions of the Mulan PSL v2.
    You may obtain a copy of Mulan PSL v2 at:
             http://license.coscl.org.cn/MulanPSL2
