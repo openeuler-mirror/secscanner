531fda3df95b6981cf9465e98b2dee1a04c5371d
pengyuan-yewu <pengyuan_yewu@cmss.chinamobile.com>
2023-09-13 16:12:03 +0800

    add item rpfilter

---
+++ secScanner/bsc_check/C32_rpfilter.py
@@ -0,0 +1,41 @@
+import logging
+import re
+from secScanner.gconfig import *
+from secScanner.lib.function import InsertSection, Display
+from secScanner.lib.TextInfo import *
+
+logger = logging.getLogger("secscanner")
+
+
+def C32_rpfilter():
+    InsertSection("check the reverse path filtering")
+    all_set = 'unset'
+    default_set = 'unset'
+    with open("/etc/sysctl.conf", "r") as file:
+        lines = file.readlines()
+        for line in lines:
+            if re.match('net.ipv4.conf.all.rp_filter', line) and not re.match('^#|^$', line):
+                all_set = 'wrong'
+                temp = line.split('=')
+                if len(temp) == 2 and temp[1] == '1':
+                    all_set = 'right'
+            if re.match('net.ipv4.conf.default.rp_filter', line) and not re.match('^#|^$', line):
+                default_set = 'wrong'
+                temp = line.split('=')
+                if len(temp) == 2 and temp[1] == '1':
+                    default_set = 'right'
+    if all_set == 'unset' and default_set == 'unset':
+        with open(RESULT_FILE, "a") as file:
+            file.write("\nC33\n")
+        logger.warning("WRN_C32_01: %s", WRN_C32_01)
+        logger.warning("SUG_C32: %s", SUG_C32)
+        Display("- No reverse path filtering config set...", "WARNING")
+    elif all_set == 'right' and default_set == 'right':
+        logger.info("Has reverse path filtering set, checking OK")
+        Display("- Check the reverse path filtering set...", "OK")
+    else:
+        with open(RESULT_FILE, "a") as file:
+            file.write("\nC33\n")
+        logger.warning("WRN_C32_02: %s", WRN_C32_02)
+        logger.warning("SUG_C32: %s", SUG_C32)
+        Display("- Wrong reverse path filtering config set...", "WARNING")
---
+++ secScanner/bsc_set/S32_rpfilter.py
@@ -0,0 +1,52 @@
+import os
+import re
+from secScanner.lib import *
+from secScanner.gconfig import *
+import shutil
+logger = logging.getLogger("secscanner")
+
+
+def S32_rpfilter():
+    set_rp_filter = seconf.get('basic', 'set_rp_filter')
+    InsertSection("Set the reverse path filtering...")
+    if set_rp_filter == 'yes':
+        if os.path.exists('/etc/sysctl.conf') and not os.path.exists('/etc/sysctl.conf_bak'):
+            shutil.copy2('/etc/sysctl.conf', '/etc/sysctl.conf_bak')
+        if os.path.exists('/etc/sysctl.conf'):
+            rp_filter = 0
+            with open('/etc/sysctl.conf', 'r') as read_file:
+                lines = read_file.readlines()
+                for line in lines:
+                    if not re.match('#|$', line) and re.search('rp_filter', line):
+                        rp_filter = 1
+            if rp_filter == 0:
+                with open('/etc/sysctl.conf', 'a') as add_file:
+                    add_file.write('\nnet.ipv4.conf.all.rp_filter=1\n')
+                    add_file.write('\nnet.ipv4.conf.default.rp_filter=1\n')
+            else:
+                with open('/etc/sysctl.conf', 'w') as write_file:
+                    for line in lines:
+                        if not re.match('#|$', line) and re.search('rp_filter', line):
+                            write_file.write('net.ipv4.conf.all.rp_filter=1\n')
+                            write_file.write('net.ipv4.conf.default.rp_filter=1\n')
+                        else:
+                            write_file.write(line)
+
+            IS_EXIST = 0
+            with open('/etc/sysctl.conf', 'r') as read_file:
+                lines = read_file.readlines()
+                for line in lines:
+                    if not re.match('#|$', line) and re.search('rp_filter', line):
+                        IS_EXIST = 1
+            if IS_EXIST == 1:
+                logger.info("Set the reverse path filtering, checking ok")
+                Display("- Set the reverse path filtering...", "FINISHED")
+            else:
+                logger.info("Set the reverse path filtering failed")
+                Display("- Set the reverse path filtering...", "FAILED")
+        else:
+            logger.info("Set the reverse path filtering failed")
+            Display("- No filepath /etc/sysctl.conf...", "FAILED")
+
+    else:
+        Display("- Skip disable the reverse path filtering due to config file...", "SKIPPING")
