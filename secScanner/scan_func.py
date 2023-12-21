4d5f1d20f60100bed44fbbe7e1f88b3c38c29e24
wwz <wangweize_yewu@cmss.chinamobile.com>
2023-12-21 10:08:38 +0000

    update secScanner/scan_func.py.
    Signed-off-by: wwz <wangweize_yewu@cmss.chinamobile.com>

--- secScanner/scan_func.py
+++ secScanner/scan_func.py
@@ -101,7 +101,8 @@ def scan_check_sys():
                     print(f"Module {module_name} does not have the required function: {e}")
                     sys.exit(1)
                 logger.info("===---------------------------------------------------------------===\n")
-
+   report.warning_results()
+   
 # check the system rootkit
 def scan_check_rootkit():
     # clear the counter, make this function re-call-able.
