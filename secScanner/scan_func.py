c0bba4d0b65ef68db937b4337562c2a2f1aecd1e
pengyuan-yewu <pengyuan_yewu@cmss.chinamobile.com>
2024-01-18 13:06:21 +0800

    modify how to start rsyslog.service

--- secScanner/scan_func.py
+++ secScanner/scan_func.py
@@ -233,10 +233,11 @@ def scan_fix_sys():
         print("")
 
     else:
-        start_service('rsyslog')
-        print("")
-        print(f"{GREEN} Finish start rsyslog.service{NORMAL}")
-        print("")
+        if subprocess.call(['systemctl', 'is-enable', 'rsyslog'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL) == 0:
+            start_service('rsyslog')
+            print("")
+            print(f"{GREEN} Finish start rsyslog.service{NORMAL}")
+            print("")
 
 
     print("")
