3ea8dc1fd877d639d0f8af8319853516550460c2
fanchun <fanchun_yewu@cmss.chinamobile.com>
2024-11-08 10:11:22 +0800

    Update unit test for C38

--- secScanner/tests/enhance/basic/check/TestC38_limitUserResources.py
+++ secScanner/tests/enhance/basic/check/TestC38_limitUserResources.py
@@ -48,6 +48,32 @@ class TestC38_limitUserResources(unittest.TestCase):
             mock_logger.info.assert_called_once_with("The system has limit of system resources, checking ok")
             mock_display.assert_called_with("- Check if the limit of system resources is ok...", "OK")
 
+    def test_missing_some_limits(self):
+        # 创建mock_open对象并设置side_effect以根据文件名返回不同内容
+        m_open = mock_open()
+        limits_conf_data = "* soft stack 10240\n* hard nproc 200\n"
+        pam_data = "session required /lib/security/pam_limits.so\n"
+        file_effects = {'/etc/security/limits.conf': mock_open(read_data=limits_conf_data).return_value,
+                        '/etc/pam.d/login': mock_open(read_data=pam_data).return_value,
+                        '/var/log/secScanner/check_result.relt': mock_open(read_data="").return_value}
+        
+        def side_effect(file_name, *args, **kwargs):
+            return file_effects[file_name]
+        
+        m_open.side_effect = side_effect
+
+        # 使用patch的side_effect功能模拟open
+        with patch('builtins.open', m_open), \
+             patch('secScanner.enhance.basic.check.C38_limitUserResources.InsertSection') as mock_insert, \
+             patch('secScanner.enhance.basic.check.C38_limitUserResources.logger') as mock_logger, \
+             patch('secScanner.enhance.basic.check.C38_limitUserResources.Display') as mock_display, \
+             patch('os.path.exists', return_value=True):
+            C38_limitUserResources()
+
+            mock_logger.warning.assert_any_call("WRN_C38: %s", WRN_C38)
+            mock_logger.warning.assert_any_call("SUG_C38: %s", SUG_C38)
+            mock_display.assert_called_with("- This system has no limit of system resources...", "WARNING")
+
 if __name__ == '__main__':
     unittest.main()
 
