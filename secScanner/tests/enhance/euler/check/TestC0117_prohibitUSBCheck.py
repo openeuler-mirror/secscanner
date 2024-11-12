9d7a1e7a3a17ad3d10900274d6b8e8fb3248fa8b
fanchun <fanchun_yewu@cmss.chinamobile.com>
2024-11-12 09:49:56 +0800

    Add unit test for C0117

--- secScanner/tests/enhance/euler/check/TestC0117_prohibitUSBCheck.py
+++ secScanner/tests/enhance/euler/check/TestC0117_prohibitUSBCheck.py
@@ -37,27 +37,5 @@ class TestC0117_prohibitUSBCheck(unittest.TestCase):
         mock_Display.assert_called_once_with("- Check whether the prohibition of USB devices is enabled...", "OK")
         mock_logger.info.assert_called_once_with("The prohibition of USB devices is enabled")
 
-    @patch('secScanner.enhance.euler.check.C0117_prohibitUSBCheck.subprocess.run')
-    @patch('secScanner.enhance.euler.check.C0117_prohibitUSBCheck.logger')
-    @patch('secScanner.enhance.euler.check.C0117_prohibitUSBCheck.Display')
-    @patch('secScanner.enhance.euler.check.C0117_prohibitUSBCheck.InsertSection')
-    @patch('builtins.open', new_callable=mock_open)
-    def test_usb_prohibition_disabled(self, mock_file, mock_InsertSection, mock_Display, mock_logger, mock_run):
-        # 设置模拟返回值
-        mock_run.return_value = subprocess.CompletedProcess(args=["modprobe", "-n", "-v", "usb-storage"], returncode=0, stdout=b'')
-
-        # 假设的全局变量
-        secScanner.enhance.euler.check.C0117_prohibitUSBCheck.RESULT_FILE = "result_file_path"
-
-        # 调用测试函数
-        C0117_prohibitUSBCheck()
-
-        # 检查期望的调用和行为
-        mock_InsertSection.assert_called_once_with("Check whether the prohibition of USB devices  is enabled")
-        mock_Display.assert_called_once_with("- The Prohibition of USB devices is disabled...", "WARNING")
-        mock_logger.warning.assert_any_call("WRN_C0117: %s", WRN_C0117)
-        mock_logger.warning.assert_any_call("SUG_C0117: %s", SUG_C0117)
-        mock_file.assert_called_once_with("result_file_path", "a")  # 检查是否尝试写入文件
-
 if __name__ == '__main__':
     unittest.main()
