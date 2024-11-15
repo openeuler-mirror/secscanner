699f7c911f234fad0d83664fa5ab5cd9b962139a
fanchun <fanchun_yewu@cmss.chinamobile.com>
2024-11-15 18:07:44 +0800

    Update unit test for C0227

--- secScanner/tests/enhance/euler/check/TestC0227_TMOUT.py
+++ secScanner/tests/enhance/euler/check/TestC0227_TMOUT.py
@@ -39,6 +39,16 @@ class TestC0227_TMOUT(unittest.TestCase):
         mock_logger.warning.assert_any_call("SUG_C0227: %s", SUG_C0227)
         mock_display.assert_any_call("- Wrong TMOUT set, must less than 300...", "WARNING")
 
+    @patch("secScanner.enhance.euler.check.C0227_TMOUT.InsertSection")
+    @patch("secScanner.enhance.euler.check.C0227_TMOUT.open", new_callable=mock_open, read_data="# TMOUT setting is commented out")
+    @patch("secScanner.enhance.euler.check.C0227_TMOUT.logger")
+    @patch("secScanner.enhance.euler.check.C0227_TMOUT.Display")
+    def test_tmout_not_set(self, mock_display, mock_logger, mock_file, mock_insert):
+        secScanner.enhance.euler.check.C0227_TMOUT.C0227_TMOUT()
+        mock_logger.warning.assert_any_call("WRN_C0227_01: %s", WRN_C0227_01)
+        mock_logger.warning.assert_any_call("SUG_C0227: %s", SUG_C0227)
+        mock_display.assert_any_call("- No TMOUT set...", "WARNING")
+
 if __name__ == '__main__':
     unittest.main()
 
