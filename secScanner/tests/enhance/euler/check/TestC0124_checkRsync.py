d124337fcdb6ffb8a2b4d981db5cc68168ca5e72
baixu <baixu_yewu@cmss.chinamobile.com>
2024-11-19 01:25:52 +0000

    Supplementary unit test for check euler C0124
    Signed-off-by: baixu <baixu_yewu@cmss.chinamobile.com>

--- secScanner/tests/enhance/euler/check/TestC0124_checkRsync.py
+++ secScanner/tests/enhance/euler/check/TestC0124_checkRsync.py
@@ -33,6 +33,27 @@ class TestC0124_checkRsync(unittest.TestCase):
         mock_logger.info.assert_any_call('The status of Rsync is :rsync-3.1.2-1.el7.x86_64')
         mock_logger.info.assert_any_call('The status of rsyncd is disabled')
         mock_Display.assert_called_with("- Check the status of rsyncd is disabled...","OK")
+    
+    @patch('secScanner.enhance.euler.check.C0124_checkRsync.InsertSection')
+    @patch('subprocess.getstatusoutput')
+    @patch('secScanner.enhance.euler.check.C0124_checkRsync.logger')
+    @patch('secScanner.enhance.euler.check.C0124_checkRsync.Display')
+    @patch('builtins.open', new_callable=mock_open)
+    def test_rsync_installed_enabled(self, mock_file, mock_Display, mock_logger, mock_getstatusoutput, mock_InsertSection):
+        # 模拟rsync已安装，服务已启用
+        mock_getstatusoutput.side_effect = [
+            (0, 'rsync-3.1.2-1.el7.x86_64'),
+            (0, 'enabled')
+        ]
+        # 调用测试函数
+        C0124_checkRsync()
+        mock_InsertSection.assert_called_once_with("Check the rsyncd server's status in your Linux System ")
+        mock_logger.info.assert_any_call('The status of Rsync is :rsync-3.1.2-1.el7.x86_64')
+        mock_logger.warning.assert_any_call("WRN_C0124: %s", WRN_C0124)
+        mock_logger.warning.assert_any_call("SUG_C0124: %s", SUG_C0124)
+        mock_Display.assert_called_with("- Check the status of rsyncd is enabled...","WARNING")
+        mock_file.assert_called_with(RESULT_FILE, 'a+')
+        mock_file().write.assert_called_with("\nC0124\n")
 
 if __name__ == '__main__':
     unittest.main()
