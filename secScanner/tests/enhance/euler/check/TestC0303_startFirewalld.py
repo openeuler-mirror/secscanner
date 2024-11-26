1beb4898c6f18fd1941f2036684ac132ea083b9b
baixu <baixu_yewu@cmss.chinamobile.com>
2024-11-26 01:19:27 +0000

    Add unit test for euler check C0303
    Signed-off-by: baixu <baixu_yewu@cmss.chinamobile.com>

---
+++ secScanner/tests/enhance/euler/check/TestC0303_startFirewalld.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+'''
+   Copyright (c) 2023. China Mobile(SuZhou)Software Technology Co.,Ltd. All rights reserved.
+   secScanner is licensed under Mulan PSL v2.
+   You can use this software according to the terms and conditions of the Mulan PSL v2.
+   You may obtain a copy of Mulan PSL v2 at:
+            http://license.coscl.org.cn/MulanPSL2
+   THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+   EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+   MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+   See the Mulan PSL v2 for more details.
+'''
+
+
+import unittest
+from unittest.mock import patch, mock_open
+from secScanner.lib.textInfo_euler import *
+from secScanner.enhance.euler.check.C0303_startFirewalld import C0303_startFirewalld
+import secScanner
+
+class TestC0303_startFirewalld(unittest.TestCase):
+
+    @patch('secScanner.enhance.euler.check.C0303_startFirewalld.InsertSection')
+    @patch('secScanner.enhance.euler.check.C0303_startFirewalld.logger')
+    @patch('secScanner.enhance.euler.check.C0303_startFirewalld.Display')
+    @patch('builtins.open', new_callable=mock_open)
+    @patch('subprocess.getstatusoutput')
+    def test_unexpected_error(self, mock_getstatusoutput, mock_open, mock_display, mock_logger, mock_InsertSection):
+        mock_getstatusoutput.side_effect = [
+            (1, ""),
+            (1, ""),
+            (1, "")
+        ]
+        C0303_startFirewalld()
+        mock_InsertSection.assert_any_call("Check if firewalld is launched")
+        mock_logger.error.assert_any_call("Unexpected error while checking firewalld status")
+        mock_display.assert_any_call("- Unexpected error while checking firewalld status...", "FAILED")
+
+if __name__ == '__main__':
+    unittest.main()
