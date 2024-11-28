2870160052ac000337f8baf9d09934961f1d3ba2
fanchun <fanchun_yewu@cmss.chinamobile.com>
2024-11-28 16:42:53 +0800

    Update unit test for C412

--- secScanner/tests/enhance/level3/check/TestC412_checkKexecTools.py
+++ secScanner/tests/enhance/level3/check/TestC412_checkKexecTools.py
@@ -34,5 +34,17 @@ class TestC412_checkKexecTools(unittest.TestCase):
         mock_logger.warning.assert_any_call("SUG_C412: %s", SUG_C412)
         mock_display.assert_called_with("- Check the  Kexec-tools software is installed...", "WARNING")
 
+    @patch('secScanner.enhance.level3.check.C412_checkKexecTools.InsertSection')
+    @patch('subprocess.getstatusoutput', return_value=(1, "package kexec-tools is not installed"))
+    @patch('secScanner.enhance.level3.check.C412_checkKexecTools.logger')
+    @patch('secScanner.enhance.level3.check.C412_checkKexecTools.Display')
+    def test_kexec_tools_not_installed(self, mock_display, mock_logger, mock_subprocess, mock_insert):
+        # 运行测试的函数
+        C412_checkKexecTools()
+
+        # 检查预期的日志信息是否已正确记录
+        mock_logger.info.assert_called_with("The Kexec-tools status is: package kexec-tools is not installed")
+        mock_display.assert_called_with("- Check the Kexec-tools software is uninstall...", "OK")
+
 if __name__ == '__main__':
     unittest.main()
