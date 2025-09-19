3709ccb6f0fd2f3bf0a62d11cf5f22db0e1e3597
wwz <wangweize_yewu@cmss.chinamobile.com>
2025-09-19 09:45:38 +0000

    add euler baseline S0337
    Signed-off-by: wwz <wangweize_yewu@cmss.chinamobile.com>

---
+++ secScanner/secScanner/enhance/euler/set/S0337_delSSHpresets2.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+'''
+   Copyright (c) 2023. China Mobile(SuZhou)Software Technology Co.,Ltd. All rights reserved.
+   secScanner is licensed under Mulan PSL v2.
+   You can use this software according to the terms and conditions of the Mulan PSL v2.
+   You may obtain a copy of Mulan PSL v2 at:
+            http://license.coscl.org.cn/MulanPSL2
+   THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+   EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+   MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+   See the Mulan PSL v2 for more details.
+'''
+
+
+import subprocess
+import logging
+import os
+#import shutil
+from secScanner.lib import *
+from secScanner.gconfig import *
+logger = logging.getLogger("secscanner")
+
+def S0337_delSSHpresets2():
+    InsertSection("Delete other presets of ssh...")
+    set_delSSHpresets2 = seconf.get('euler', 'set_delSSHpresets2')
+    if set_delSSHpresets2 == 'yes':
+        ret, result = subprocess.getstatusoutput("find /home/ /root/ -name known_hosts")
+        if ret == 0 and result == '':
+            logger.info("Not found ssh presets in /home/ /root/")
+            Display("- Not found ssh presets in /home/ /root/...", "FINISHED")
+        elif ret == 0 and "known_hosts" in result:
+            files_list = result.split("\n")
+            for single_file in files_list:
+                if os.path.exists(single_file):
+                    os.remove(single_file)
+            ret, result = subprocess.getstatusoutput("find /home/ /root/ -name known_hosts")
+            if ret == 0 and result == '':
+                logger.info("Delete found ssh presets in /home/ /root/")
+                Display("- Delete found ssh presets in /home/ /root/...", "FINISHED")
+            else:
+                logger.error("Fail to delete ssh presets in /home/ /root/")
+                Display("- Fail to delete ssh presets in /home/ /root/...", "FAILED")
+        else:
+            logger.error("Error occured while finding ssh presets in /home/ /root/")
+            Display("- Error occured while finding ssh presets in /home/ /root/...", "FAILED")
+    else:
+        Display("- Skip delete presets of ssh due to config file", "SKIPPING")
