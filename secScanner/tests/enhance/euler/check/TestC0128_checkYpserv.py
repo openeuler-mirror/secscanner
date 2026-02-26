bfbba42f0e266522fbc9b2c0b34945385c4cec3b
baixu_yewu <baixu_yewu@cmss.chinamobile.com>
2026-02-26 15:48:51 +0800

    update unit test
    Signed-off-by: baixu_yewu <baixu_yewu@cmss.chinamobile.com>

--- secScanner/tests/enhance/euler/check/TestC0128_checkYpserv.py
+++ secScanner/tests/enhance/euler/check/TestC0128_checkYpserv.py
@@ -63,6 +63,17 @@ class TestC0128_checkYpserv(unittest.TestCase):
         mock_logger.warning.assert_any_call("SUG_C0128: %s", SUG_C0128)
         mock_display.assert_called_with("- Check the ypserv software is installed...", "WARNING")        
         mock_file.assert_any_call("result_file_path", "a+")  # 检查是否尝试写入文件
+    
+    @patch('secScanner.enhance.euler.check.C0128_checkYpserv.os.path.exists')
+    @patch('secScanner.enhance.euler.check.C0128_checkYpserv.os.path.getsize')
+    @patch('secScanner.enhance.euler.check.C0128_checkYpserv.InsertSection')
+    @patch('subprocess.getstatusoutput', return_value=(1, 'uninstalled'))
+    @patch('secScanner.enhance.euler.check.C0128_checkYpserv.logger')
+    @patch('secScanner.enhance.euler.check.C0128_checkYpserv.Display')
+    @patch('builtins.open', new_callable=mock_open)
+    def test_001_ret1_insert_section_once(self, mock_file, mock_display, mock_logger, mock_getstatusoutput, mock_InsertSection, mock_getsize, mock_exists):
+        C0128_checkYpserv()
+        self.assertEqual(mock_InsertSection.call_count, 1)
 
 if __name__ == '__main':
     unittest.main()   
