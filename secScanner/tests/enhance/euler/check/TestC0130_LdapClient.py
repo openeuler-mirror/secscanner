f5843c8344622b63756e84e8daf167fb6c4c6492
baixu_yewu <baixu_yewu@cmss.chinamobile.com>
2026-03-12 13:55:09 +0800

    update TestC0130
    Signed-off-by: baixu_yewu <baixu_yewu@cmss.chinamobile.com>

--- secScanner/tests/enhance/euler/check/TestC0130_LdapClient.py
+++ secScanner/tests/enhance/euler/check/TestC0130_LdapClient.py
@@ -109,6 +109,17 @@ class TestC0130_LdapClient(unittest.TestCase):
         args, _ = mock_display.call_args
         self.assertEqual(args[0], "- Check the openldap-clients software is uninstall...")
         self.assertEqual(args[1], "OK")
+    
+    @patch('secScanner.enhance.euler.check.C0130_LdapClient.os.path.exists')
+    @patch('secScanner.enhance.euler.check.C0130_LdapClient.os.path.getsize')
+    @patch('secScanner.enhance.euler.check.C0130_LdapClient.InsertSection')
+    @patch('subprocess.getstatusoutput', return_value=(2, 'not found'))
+    @patch('secScanner.enhance.euler.check.C0130_LdapClient.logger')
+    @patch('secScanner.enhance.euler.check.C0130_LdapClient.Display')
+    @patch('builtins.open', new_callable=mock_open)
+    def test_005_ret2_ok(self, mock_file, mock_display, mock_logger, mock_getstatusoutput, mock_InsertSection, mock_getsize, mock_exists):
+        C0130_LdapClient()
+        mock_display.assert_called_once_with("- Check the openldap-clients software is uninstall...", "OK")
 
 
 if __name__ == '__main':
