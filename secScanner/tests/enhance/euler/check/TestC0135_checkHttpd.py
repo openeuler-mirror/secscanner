1b4817f63aa71f19a1a1970a0e9e728c4224bef1
baixu_yewu <baixu_yewu@cmss.chinamobile.com>
2026-03-16 10:07:36 +0800

    update TestC0135
    Signed-off-by: baixu_yewu <baixu_yewu@cmss.chinamobile.com>

--- secScanner/tests/enhance/euler/check/TestC0135_checkHttpd.py
+++ secScanner/tests/enhance/euler/check/TestC0135_checkHttpd.py
@@ -121,6 +121,17 @@ class TestC0135_checkHttpd(unittest.TestCase):
     def test_005_ret2_ok(self, mock_file, mock_display, mock_logger, mock_getstatusoutput, mock_InsertSection, mock_getsize, mock_exists):
         C0135_checkHttpd()
         mock_display.assert_called_once_with("- Check the httpd software is uninstall...", "OK")
+    
+    @patch('secScanner.enhance.euler.check.C0135_checkHttpd.os.path.exists')
+    @patch('secScanner.enhance.euler.check.C0135_checkHttpd.os.path.getsize')
+    @patch('secScanner.enhance.euler.check.C0135_checkHttpd.InsertSection')
+    @patch('subprocess.getstatusoutput', return_value=(255, 'error'))
+    @patch('secScanner.enhance.euler.check.C0135_checkHttpd.logger')
+    @patch('secScanner.enhance.euler.check.C0135_checkHttpd.Display')
+    @patch('builtins.open', new_callable=mock_open)
+    def test_006_ret255_ok(self, mock_file, mock_display, mock_logger, mock_getstatusoutput, mock_InsertSection, mock_getsize, mock_exists):
+        C0135_checkHttpd()
+        mock_display.assert_called_once_with("- Check the httpd software is uninstall...", "OK")
 
 if __name__ == '__main':
     unittest.main()    
