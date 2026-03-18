3e01bc74bc3f9e8a8094f5b03012492c540ce551
baixu_yewu <baixu_yewu@cmss.chinamobile.com>
2026-03-18 11:27:39 +0800

    update TestC0129
    Signed-off-by: baixu_yewu <baixu_yewu@cmss.chinamobile.com>

--- secScanner/tests/enhance/euler/check/TestC0129_checkYpbind.py
+++ secScanner/tests/enhance/euler/check/TestC0129_checkYpbind.py
@@ -144,6 +144,17 @@ class TestC0129_checkYpbind(unittest.TestCase):
             C0129_checkYpbind()
         except Exception as e:
             self.fail(f"C0129_checkYpbind() raised an exception: {e}")
+        
+    @patch('secScanner.enhance.euler.check.C0129_checkYpbind.os.path.exists')
+    @patch('secScanner.enhance.euler.check.C0129_checkYpbind.os.path.getsize')
+    @patch('secScanner.enhance.euler.check.C0129_checkYpbind.InsertSection')
+    @patch('subprocess.getstatusoutput', return_value=(1, 'uninstalled'))
+    @patch('secScanner.enhance.euler.check.C0129_checkYpbind.logger')
+    @patch('secScanner.enhance.euler.check.C0129_checkYpbind.Display')
+    @patch('builtins.open', new_callable=mock_open)
+    def test_008_ret1_logger_info_exact(self, mock_file, mock_display, mock_logger, mock_getstatusoutput, mock_InsertSection, mock_getsize, mock_exists):
+        C0129_checkYpbind()
+        mock_logger.info.assert_called_once_with("The Ypbind status is: uninstalled")
 
 if __name__ == '__main':
     unittest.main()    
