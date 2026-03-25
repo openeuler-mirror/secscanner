79431e0c560f5f50ac086aea9679df6ced387430
baixu_yewu <baixu_yewu@cmss.chinamobile.com>
2026-03-25 11:19:26 +0800

    update TestC0117
    Signed-off-by: baixu_yewu <baixu_yewu@cmss.chinamobile.com>

--- secScanner/tests/enhance/euler/check/TestC0117_prohibitUSBCheck.py
+++ secScanner/tests/enhance/euler/check/TestC0117_prohibitUSBCheck.py
@@ -188,6 +188,15 @@ class TestC0117_prohibitUSBCheck(unittest.TestCase):
         secScanner.enhance.euler.check.C0117_prohibitUSBCheck.RESULT_FILE = "result_file_path"
         C0117_prohibitUSBCheck()
         self.assertEqual(mock_InsertSection.call_count, 1)
+    
+    @patch('secScanner.enhance.euler.check.C0117_prohibitUSBCheck.subprocess.run')
+    @patch('secScanner.enhance.euler.check.C0117_prohibitUSBCheck.logger')
+    @patch('secScanner.enhance.euler.check.C0117_prohibitUSBCheck.Display')
+    @patch('secScanner.enhance.euler.check.C0117_prohibitUSBCheck.InsertSection')
+    def test_013_logger_warning_not_called_when_enabled(self, mock_InsertSection, mock_Display, mock_logger, mock_run):
+        mock_run.return_value = subprocess.CompletedProcess(args=["modprobe", "-n", "-v", "usb-storage"], returncode=0, stdout=b'install /bin/true')
+        C0117_prohibitUSBCheck()
+        mock_logger.warning.assert_not_called()
 
 if __name__ == '__main__':
     unittest.main()
