e4ef42251b4f4a55ae83895a9ca007c899cb76e6
baixu_yewu <baixu_yewu@cmss.chinamobile.com>
2026-03-26 10:33:24 +0800

    update TestC0127
    Signed-off-by: baixu_yewu <baixu_yewu@cmss.chinamobile.com>

--- secScanner/tests/enhance/euler/check/TestC0127_checkCups.py
+++ secScanner/tests/enhance/euler/check/TestC0127_checkCups.py
@@ -204,6 +204,18 @@ class TestC0127_checkCups(unittest.TestCase):
         secScanner.enhance.euler.check.C0127_checkCups.RESULT_FILE = "result_file_path"
         C0127_checkCups()
         self.assertEqual(mock_logger.warning.call_count, 2)
+    
+    @patch('secScanner.enhance.euler.check.C0127_checkCups.os.path.exists')
+    @patch('secScanner.enhance.euler.check.C0127_checkCups.os.path.getsize')
+    @patch('secScanner.enhance.euler.check.C0127_checkCups.InsertSection')
+    @patch('subprocess.getstatusoutput', return_value=(0, 'installed'))
+    @patch('secScanner.enhance.euler.check.C0127_checkCups.logger')
+    @patch('secScanner.enhance.euler.check.C0127_checkCups.Display')
+    @patch('builtins.open', new_callable=mock_open)
+    def test_013_ret0_file_open_aplus(self, mock_file, mock_display, mock_logger, mock_getstatusoutput, mock_InsertSection, mock_getsize, mock_exists):
+        secScanner.enhance.euler.check.C0127_checkCups.RESULT_FILE = "result_file_path"
+        C0127_checkCups()
+        mock_file.assert_called_with("result_file_path", "a+")
 
 if __name__ == '__main':
     unittest.main()   
