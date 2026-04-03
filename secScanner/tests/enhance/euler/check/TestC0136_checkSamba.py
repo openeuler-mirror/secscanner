9a3ca77f345243681dbb95ed6617ea3b01a136b6
baixu_yewu <baixu_yewu@cmss.chinamobile.com>
2026-04-03 14:33:54 +0800

    update TestC0136
    Signed-off-by: baixu_yewu <baixu_yewu@cmss.chinamobile.com>

--- secScanner/tests/enhance/euler/check/TestC0136_checkSamba.py
+++ secScanner/tests/enhance/euler/check/TestC0136_checkSamba.py
@@ -206,6 +206,18 @@ class TestC0136_checkSamba(unittest.TestCase):
         secScanner.enhance.euler.check.C0136_checkSamba.RESULT_FILE = "result_file_path"
         C0136_checkSamba()
         self.assertEqual(mock_logger.warning.call_count, 2)
+    
+    @patch('secScanner.enhance.euler.check.C0136_checkSamba.os.path.exists')
+    @patch('secScanner.enhance.euler.check.C0136_checkSamba.os.path.getsize')
+    @patch('secScanner.enhance.euler.check.C0136_checkSamba.InsertSection')
+    @patch('subprocess.getstatusoutput', return_value=(0, 'installed'))
+    @patch('secScanner.enhance.euler.check.C0136_checkSamba.logger')
+    @patch('secScanner.enhance.euler.check.C0136_checkSamba.Display')
+    @patch('builtins.open', new_callable=mock_open)
+    def test_013_ret0_file_open_aplus(self, mock_file, mock_display, mock_logger, mock_getstatusoutput, mock_InsertSection, mock_getsize, mock_exists):
+        secScanner.enhance.euler.check.C0136_checkSamba.RESULT_FILE = "result_file_path"
+        C0136_checkSamba()
+        mock_file.assert_called_with("result_file_path", "a+")
 
 if __name__ == '__main':
     unittest.main()   
