ab2d23bceba777d41abf31380fd1075f4192bb06
lamerpoet <xuzheng_yewu@cmss.chinamobile.com>
2026-07-05 22:28:23 +0800

    Clean non-English test comments

--- secScanner/tests/enhance/level3/check/TestC114_loginDefs.py
+++ secScanner/tests/enhance/level3/check/TestC114_loginDefs.py
@@ -63,7 +63,7 @@ class TestC114_loginDefs(unittest.TestCase):
         mock_logger.warning.assert_any_call("WRN_C114_02: %s", WRN_C114_02)
         mock_display.assert_any_call("- PASS_MAX_DAYS value is null...", "WARNING")
 
-# 如果是作为脚本运行，自动执行测试
+# Mock test setup.
 if __name__ == '__main__':
     unittest.main()
 
