33a709d345bae40aafde7475a9fefec6ea662325
lamerpoet <xuzheng_yewu@cmss.chinamobile.com>
2026-07-05 22:27:23 +0800

    Clean non-English test comments

--- secScanner/tests/enhance/euler/check/TestC0123_debugShell.py
+++ secScanner/tests/enhance/euler/check/TestC0123_debugShell.py
@@ -26,7 +26,7 @@ class TestC0123_debugShell(unittest.TestCase):
     @patch('secScanner.enhance.euler.check.C0123_debugShell.Display')
     def test_debug_shell_server_disabled(self, mock_display, mock_logger, mock_getstatusoutput, mock_InsertSection):
         
-        # 调用测试函数
+        # Mock test setup.
         C0123_debugShell()
         mock_InsertSection.assert_called_once_with("Check the debug-shell server's status in your Linux System ")
         mock_logger.info.assert_called_once_with("The status of debug-shell is disabled")
@@ -39,16 +39,16 @@ class TestC0123_debugShell(unittest.TestCase):
     @patch('secScanner.enhance.euler.check.C0123_debugShell.Display')
     @patch('builtins.open', new_callable=mock_open)
     def test_debug_shell_server_enabled(self, mock_file, mock_display, mock_logger, mock_getstatusoutput, mock_InsertSection):
-        # 假设的全局变量
-        secScanner.enhance.euler.check.C0123_debugShell.RESULT_FILE = "result_file_path"  # 假设的结果文件路径
+        # Mock test setup.
+        secScanner.enhance.euler.check.C0123_debugShell.RESULT_FILE = "result_file_path"  # Mock test setup.
         
-        # 调用测试函数
+        # Mock test setup.
         C0123_debugShell()
         
         mock_InsertSection.assert_called_once_with("Check the debug-shell server's status in your Linux System ") 
         mock_display.assert_called_once_with("- Check the status of debug-shell is enabled...", "WARNING")
         mock_logger.warning.assert_any_call("WRN_C0123: %s", WRN_C0123)
-        mock_file.assert_called_once_with("result_file_path", "a+")  # 检查是否尝试写入文件
+        mock_file.assert_called_once_with("result_file_path", "a+")  # Mock test setup.
 
     @patch('secScanner.enhance.euler.check.C0123_debugShell.InsertSection')
     @patch('subprocess.getstatusoutput', return_value=(1, 'disabled'))
